e80d792d15f7806783d7f86f6a067268fe4da64b
Nicolas Marcelino <141439592+nicolasmarcelino@users.noreply.github.com>
2025-08-27 21:35:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const limiteTarefas = 10;
+const input = document.getElementById('novaTarefa');
+const btnAdd = document.getElementById('btnAdd');
+const lista = document.getElementById('listaTarefas');
+const msgErro = document.getElementById('msgErro');
+
+btnAdd.addEventListener('click', () => {
+  const tarefa = input.value.trim();
+
+  if (tarefa === '') {
+    msgErro.textContent = 'Digite sua tarefa';
+    return;
+  }
+
+  const tarefasAtuais = lista.getElementsByTagName('li').length;
+
+  if (tarefasAtuais >= limiteTarefas) {
+    msgErro.textContent = `Você atingiu o limite de ${limiteTarefas} tarefas.`;
+    return;
+  }
+
+  msgErro.textContent = '';
+
+  const li = document.createElement('li');
+  li.textContent = tarefa;
+
+  // Cria botão remover
+  const btnRemover = document.createElement('button');
+  btnRemover.textContent = 'Delete';
+  btnRemover.addEventListener('click', () => {
+    lista.removeChild(li);
+    msgErro.textContent = ''; // limpa mensagem se houver
+  });
+
+  li.appendChild(btnRemover);
+  lista.appendChild(li);
+
+  input.value = '';
+});
